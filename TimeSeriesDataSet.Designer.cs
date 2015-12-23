250d75c86f4811fbbba947bedfa7fede4b134dd3
tjrocha <jrocha@usbr.gov>
2015-12-23 15:22:48 -0800

    fixed error with backward compatibility for *.hdb config files

--- TimeSeriesDataSet.Designer.cs
+++ TimeSeriesDataSet.Designer.cs
@@ -1579,7 +1579,7 @@ namespace HdbPoet {
                         return ((decimal)(this[this.tableSeries.model_run_idColumn]));
                     }
                     catch (global::System.InvalidCastException e) {
-                        throw new global::System.Data.StrongTypingException("The value for column \'model_run_id\' in table \'Series\' is DBNull.", e);
+                        return ((decimal)0);
                     }
                 }
                 set {
