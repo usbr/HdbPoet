33cc2c4528fa381bc881c5725a12c3b370bf7678
ktarbet <ktarbet@gmail.com>
2015-12-24 10:40:45 -0700

    3.2.21

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
-[assembly: AssemblyVersion("3.2.20.0")]
+[assembly: AssemblyVersion("3.2.21.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
