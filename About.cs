9a9b2b2e92becb9c4693919aee6bbbd429c6c02a
tjrocha <jrocha@usbr.gov>
2017-01-10 15:30:39 -0800

    Incremented 'Patch' version integer and updated the About page with a link to GitHub.

--- About.cs
+++ About.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Diagnostics;
 using System.Collections;
 using System.ComponentModel;
 using System.Windows.Forms;
@@ -13,15 +14,18 @@ namespace HdbPoet
 	{
 		private System.Windows.Forms.Label labelVersion;
 		private System.Windows.Forms.Button buttonOk;
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
+        private LinkLabel linkLabel1;
+        private RichTextBox richTextBox1;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
 
 		public About()
 		{
 			InitializeComponent();
-			this.labelVersion.Text = "Version "+Application.ProductVersion;
+            this.labelVersion.Text = "Version " + Application.ProductVersion;
 		}
 
 		/// <summary>
@@ -48,29 +52,56 @@ namespace HdbPoet
 		{
             this.labelVersion = new System.Windows.Forms.Label();
             this.buttonOk = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // labelVersion
             // 
-            this.labelVersion.Location = new System.Drawing.Point(8, 48);
+            this.labelVersion.Location = new System.Drawing.Point(2, 5);
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(320, 32);
+            this.labelVersion.Size = new System.Drawing.Size(337, 17);
             this.labelVersion.TabIndex = 0;
             this.labelVersion.Text = "version number here..";
             // 
             // buttonOk
             // 
-            this.buttonOk.Location = new System.Drawing.Point(264, 136);
+            this.buttonOk.Location = new System.Drawing.Point(264, 105);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(75, 23);
             this.buttonOk.TabIndex = 1;
             this.buttonOk.Text = "Ok";
             this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(2, 25);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(261, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Let us know what you think of HDB POET on GitHub.";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(5, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 55);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "- Submit feedback, bug reports, issues, and enhanment requests.\n- Help us build a" +
+    "nd maintain the software.\n- Visit the GitHub wiki for a brief tutorial on how to" +
+    " use the software.";
+            // 
             // About
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(344, 174);
+            this.ClientSize = new System.Drawing.Size(344, 139);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.labelVersion);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -80,6 +111,7 @@ namespace HdbPoet
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "About HDB Poet";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 		#endregion
@@ -88,5 +120,11 @@ namespace HdbPoet
 		{
 			Close();
 		}
-	}
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Send the URL to the operating system.
+            Process.Start(@"https://github.com/usbr/HdbPoet" as string);
+        }
+    }
 }
