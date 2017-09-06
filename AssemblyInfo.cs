e73bd101edff107ae4ebbb8af816553b132a2715
tjrocha <jrocha@usbr.gov>
2017-09-06 08:32:07 -0700

    Increment minor version number

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
-[assembly: AssemblyVersion("3.3.4.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
