034855f1b92a55728c2c2ab7ef0bbe4f6246eb84
tjrocha <jrocha@usbr.gov>
2017-09-10 08:10:12 -0700

    Initial commit for #37

---
+++ Tests/HdbPoetTestConnections.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HdbPoet.Tests
+{
+    /// <summary>
+    /// Test HDB connection and user related funtionality
+    /// </summary>
+    [TestFixture]
+    class HdbPoetTestConnectivity
+    {
+        private void TestConnectionLC()
+        {
+
+        }
+
+        private void TestConnectionUC()
+        {
+
+        }
+
+        private void TestConnectionYAO()
+        {
+
+        }
+
+        private void TestConnectionECAO()
+        {
+
+        }
+
+        private void TestAdminOptionsVisible()
+        {
+
+        }
+
+        private void TestAdminOptionsHidden()
+        {
+
+        }
+
+
+    }
+}
---
+++ Tests/HdbPoetTestIO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HdbPoet.Tests
+{
+    /// <summary>
+    /// Test Create-Replace-Update-Delete related functionality
+    /// </summary>
+    [TestFixture]
+    class HdbPoetTestIO
+    {
+        private void TestRead()
+        {
+
+        }
+
+        private void TestWrite()
+        {
+
+        }
+
+        private void TestDelete()
+        {
+
+        }
+
+        private void TestOverwriteFlagAdd()
+        {
+
+        }
+
+        private void TestOverwriteFlagRemove()
+        {
+
+        }
+
+    }
+}
