e99996740c42e258d5355b069c6b751ceeaa2c1a
tjrocha <jrocha@usbr.gov>
2017-09-13 12:29:29 -0700

    more reorg

--- build_installer.bat
+++
@@ -1,6 +0,0 @@
-cd "C:\Users\jrocha\Documents\Visual Studio 2015\Projects\HdbPoet"
-"C:\Program Files (x86)\Inno Setup 5\iscc.exe" hdbpoet.iss
-
-::del .\output\poet_src.zip
-::zip .\output\poet_src.zip *.cs *.resx *.config *.ico *.png *.csproj
-pause
